ad9db04f8e27dd5cfa049323fd512c07bb981a83
Jesse Weisner <jesse_weisner@bcit.ca>
2019-03-21 13:11:58 -0700

    make it better

--- 60-stunnel-config.sh
+++ 60-stunnel-config.sh
@@ -6,19 +6,21 @@ if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || [[ -z "${STU
     exit 1
 fi
 
-if [[ ! -f ${STUNNEL_KEY} ]]; then
-    if [[ -f ${STUNNEL_CRT} ]]; then
-        echo >&2 "crt (${STUNNEL_CRT}) missing key (${STUNNEL_KEY})"
-        exit 1
-    fi
+if [[ "${STUNNEL_VERIFY_CHAIN}" != "no" ]] && [[ ! -f ${STUNNEL_CACERT} ]]; then
+    echo >&2 "CA cert \"${STUNNEL_CACERT}\" not found. Disabling verification."
+    export STUNNEL_VERIFY_CHAIN='no'
+fi
 
+if [[ "${STUNNEL_VERIFY_CHAIN}" != "no" ]];then
+    if [[ ! -r ${STUNNEL_KEY} ]] || [[ ! -r ${STUNNEL_CERT} ]]; then
+        echo >&2 "Cert ${STUNNEL_CERT} or key ${STUNNEL_KEY} missing. Generating new pair."
+        rm -f ${STUNNEL_KEY} ${STUNNEL_CERT}
         openssl req -x509 -nodes -rand ${RANDOM_SOURCE} -days 365 -newkey rsa:2048 \
         -keyout ${STUNNEL_KEY} -out ${STUNNEL_CERT} \
         -config /etc/stunnel/openssl.cnf
+    fi
 fi
 
-cp -v ${STUNNEL_CAFILE} /usr/local/share/ca-certificates/stunnel-ca.crt
-cp -v ${STUNNEL_CRT} /usr/local/share/ca-certificates/stunnel.crt
-update-ca-certificates
+export STUNNEL_VERIFY_CHAIN
 
 cat /etc/stunnel/stunnel.conf.template | envsubst > ${STUNNEL_CONF}
