9e20383850a3b5a6a3090ff2c2265a9b5c65529e
Jesse Weisner <jesse_weisner@bcit.ca>
2019-03-21 09:49:14 -0700

    use /dev/random as entropy source

--- 60-stunnel-config.sh
+++ 60-stunnel-config.sh
@@ -21,7 +21,7 @@ if [[ ! -f ${STUNNEL_KEY} ]]; then
         exit 1
     fi
 
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${STUNNEL_KEY} -out ${STUNNEL_CRT} \
+    openssl req -x509 -nodes -rand /dev/random -days 365 -newkey rsa:2048 -keyout ${STUNNEL_KEY} -out ${STUNNEL_CRT} \
         -config /etc/stunnel/openssl.cnf
 fi
 
